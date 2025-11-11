99a8db054b7443d49edb4ead466a527eb4a4b2de
Yonas Mekonnen <myonas886@gmail.com>
2025-11-11 15:44:53 +0300

    feat: update Docker setup for PostgreSQL integration and enhance seed script with multiple user creation

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -4,9 +4,15 @@ import * as bcrypt from 'bcrypt';
 const prisma = new PrismaClient();
 
 async function main() {
+  await prisma.user.deleteMany();
+
   const adminEmail = 'admin@example.com';
+  const userEmail = 'test@example.com';
   const existingAdmin = await prisma.user.findUnique({
     where: { email: adminEmail },
+  });
+  const existigUser = await prisma.user.findUnique({
+    where: { email: userEmail },
   });
 
   if (existingAdmin) {
@@ -14,15 +20,29 @@ async function main() {
     return;
   }
 
-  const hashedPassword = await bcrypt.hash('Admin@123', 10);
+  if (existigUser) {
+    console.log('Admin user already exists:', existigUser.email);
+    return;
+  }
 
-  const admin = await prisma.user.create({
-    data: {
-      username: 'admin',
-      email: adminEmail,
-      password: hashedPassword,
-      role: 'ADMIN',
-    },
+  const hashedPassword = await bcrypt.hash('Admin@123', 10);
+  const userPassword = await bcrypt.hash('Pass@123', 10);
+
+  const admin = await prisma.user.createMany({
+    data: [
+      {
+        username: 'admin',
+        email: adminEmail,
+        password: hashedPassword,
+        role: 'ADMIN',
+      },
+      {
+        username: 'user1',
+        email: userEmail,
+        password: userPassword,
+        role: 'CUSTOMER',
+      },
+    ],
   });
 
   console.log('✅ Admin user created:', admin);
